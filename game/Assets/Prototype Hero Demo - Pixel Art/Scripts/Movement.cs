c3596663c4d2f23fbe7e63af64a88b274d61f29c
09lkei <09lkei@brightoncollege.net>
2023-01-19 10:54:06 +0000

    Add jump

---
+++ game/Assets/Prototype Hero Demo - Pixel Art/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            rb.velocity = new Vector2(0, 10);
+        }
+    }
+}
