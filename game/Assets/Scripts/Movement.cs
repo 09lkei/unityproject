250288be7762f5154836315481ea2955fa057473
jaedai1757 <114415649+jaedai1757@users.noreply.github.com>
2023-01-23 19:25:03 +0000

    bruh

--- game/Assets/Scripts/Movement.cs
+++ game/Assets/Scripts/Movement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class Movement : MonoBehaviour
 {
@@ -10,23 +9,12 @@ public class Movement : MonoBehaviour
 
     private float HorizontalMovement = 0f;
     public float movementSpeed = 0f;
-    public Animator animator;
-    public Rigidbody2D rb;
 
     private bool jump = false;
     // Update is called once per frame
     void Update()
     {
         HorizontalMovement = Input.GetAxisRaw("Horizontal");
-        Debug.Log(HorizontalMovement);
-        if (movementSpeed<1) {
-            movementSpeed+=HorizontalMovement/1000;
-        }
-        if (movementSpeed!=0 && HorizontalMovement==0) {
-            movementSpeed=0;
-        }
-        animator.SetFloat("Speed",Math.Abs(HorizontalMovement));
-        animator.SetFloat("Horizontal",rb.velocity.y);
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
@@ -35,7 +23,7 @@ public class Movement : MonoBehaviour
 
     void FixedUpdate()
     {
-        controller.Move(movementSpeed,false,jump);
+        controller.Move(HorizontalMovement,false,jump);
         jump = false;
     }
 }
