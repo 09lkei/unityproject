022700205581ba8cad284fae42ce4f65165d9b63
jaedai1757 <114415649+jaedai1757@users.noreply.github.com>
2023-01-30 20:54:58 +0000

    i hope this works

--- game/Assets/Scripts/Character/EnemyScript.cs
+++ game/Assets/Scripts/Character/EnemyScript.cs
@@ -24,6 +24,41 @@ public class EnemyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //distacne to player
+        float distToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if(distToPlayer < agroRange)
+        {
+            //code to chase player
+            ChasePlayer();
+        }
+        else
+        {
+            //stop chasing player
+            StopChasingPlayer();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //enemy is to the left side of the player, so move right
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+            transform.localScale = new Vector2(1, 1);
+        }
+
+        else
+        {
+            //enemy is to te right side of the player, so move left
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+            transform.localScale = new Vector2(-1, 1);
+        }
+
+    }
+
+    private void StopChasingPlayer()
+    {
+        rb2d.velocity = new Vector2(0, 0);
     }
 }
