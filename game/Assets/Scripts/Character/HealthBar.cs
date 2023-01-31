7f9c7f00adc16676558700444298fe251b8091e1
Luke H-N <09lhillnorton@brightoncollege.net>
2023-01-31 13:05:13 +0000

    destroy

--- game/Assets/Scripts/Character/HealthBar.cs
+++ game/Assets/Scripts/Character/HealthBar.cs
@@ -12,6 +12,10 @@ public class HealthBar : MonoBehaviour
     public void UpdateHealthBar()
     {
         healthBarImage.fillAmount = Mathf.Clamp(player.health / player.maxHealth, 0, 1f);
+        if (player.health == 0)
+        {
+            Destroy(gameObject);
+        }
     }
 
 }
