9db476ffb1706ba2da4cb8bac23ce30c80f13659
jaedai1757 <114415649+jaedai1757@users.noreply.github.com>
2023-02-01 17:27:20 +0000

    added inkle to unity

--- game/Assets/Scripts/Enemy.cs
+++ game/Assets/Scripts/Enemy.cs
@@ -6,44 +6,47 @@ using System;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float agroRange;
     [SerializeField] private Animator animator;
     [SerializeField] private Transform self;
     [SerializeField] private CharacterController2D controller;
     [SerializeField] private Transform player;
     [SerializeField] private float maxSpeed = 0.5f;
-	[SerializeField] private float movementSpeed = 0.5f;
+    [SerializeField] private float movementSpeed = 0.5f;
     private float HorizontalMovement = 0f;
     private float currentSpeed = 0f;
     private bool jump = false;
     // Update is called once per frame
     void Start() {
-        currentSpeed=movementSpeed;
+        currentSpeed = movementSpeed;
     }
     void Update()
     {
         Debug.Log(movementSpeed);
-        if (player.position.x>self.position.x) {
-            HorizontalMovement=1f;
-        } else {
-            HorizontalMovement=-1f;
+        if (player.position.x > self.position.x)
+        {
+            HorizontalMovement = 1f;
         }
-        animator.SetFloat("Speed",Math.Abs(HorizontalMovement*currentSpeed));
-        animator.SetFloat("Vertical",rb.velocity.y);
-        
+        else
+        {
+            HorizontalMovement = -1f;
+        }
+        animator.SetFloat("Speed", Math.Abs(HorizontalMovement * currentSpeed));
+        animator.SetFloat("Vertical", rb.velocity.y);
     }
     void FixedUpdate()
     {
-        controller.Move(HorizontalMovement*currentSpeed,false,jump);
+        controller.Move(HorizontalMovement * currentSpeed, false, jump);
         jump = false;
     }
     public void takeDamage() {
-        rb.velocity = new Vector2(self.position.x-player.position.x,3);
+        rb.velocity = new Vector2(self.position.x - player.position.x, 3);
         animator.SetTrigger("Hurt");
         StartCoroutine(stun());
     }
     IEnumerator stun() {
         currentSpeed = 0;
         yield return new WaitForSeconds(3);
-        currentSpeed=movementSpeed;
+        currentSpeed = movementSpeed;
     }
 }
